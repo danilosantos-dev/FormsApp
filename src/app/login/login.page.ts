8887a6f9a9c6fc4ea9517296ea89b73bbdd4b701
LeonoraMaitan <leonora.maitan@gmail.com>
2022-11-04 20:39:36 -0300

    validação de senha e email

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    senha: ['',Validators.compose([Validators.required, Validators.minLength(6)])],
+  });
+
+  mensagensErro = {
+    email: [{tipo: 'required', aviso: 'O campo não pode estar vazio'}, {tipo: 'email', aviso: 'Digite um e-mail'}],
+    senha: [{tipo: 'required', aviso: 'O campo não pode estar vazio'}, {tipo: 'minLength', aviso: 'Deve ter no mínimo 6 dígitos'}],
+  };
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
